107afbef84d9fe83e28f6c189976adc91352cb62
Ivan Kalinin <i.kalinin@dexpa.io>
2020-11-01 01:04:33 +0300

    Add simple test

--- src/tonlib-js/tonlibjs.cpp
+++ src/tonlib-js/tonlibjs.cpp
@@ -1,14 +1,55 @@
-// clang-format off
 #include <node_version.h>
-// clang-format on
 
+//
+
+#include <auto/tl/tonlib_api_json.h>
 #include <napi.h>
 #include <node_api.h>
+#include <td/utils/JsonBuilder.h>
 #include <td/utils/Slice.h>
-#include <tonlib/ClientJson.h>
+#include <td/utils/Status.h>
+#include <td/utils/common.h>
+#include <td/utils/format.h>
+#include <td/utils/logging.h>
+#include <td/utils/port/thread_local.h>
+#include <tl/tl_json.h>
+#include <tonlib/Client.h>
+
 
 namespace tjs
 {
+static td::Result<std::pair<tonlib_api::object_ptr<tonlib_api::Function>, std::string>> to_request(td::Slice request)
+{
+    auto request_str = request.str();
+    TRY_RESULT(json_value, td::json_decode(request_str))
+    if (json_value.type() != td::JsonValue::Type::Object) {
+        return td::Status::Error("Expected an Object");
+    }
+
+    std::string extra;
+    if (has_json_object_field(json_value.get_object(), "@extra")) {
+        extra = td::json_encode<std::string>(get_json_object_field(json_value.get_object(), "@extra", td::JsonValue::Type::Null).move_as_ok());
+    }
+
+    tonlib_api::object_ptr<tonlib_api::Function> func;
+    TRY_STATUS(from_json(func, json_value))
+    return std::make_pair(std::move(func), extra);
+}
+
+static std::string from_response(const tonlib_api::Object& object, const td::string& extra)
+{
+    auto str = td::json_encode<td::string>(td::ToJson(object));
+    CHECK(!str.empty() && str.back() == '}')
+    if (!extra.empty()) {
+        str.pop_back();
+        str.reserve(str.size() + 11 + extra.size());
+        str += ",\"@extra\":";
+        str += extra;
+        str += '}';
+    }
+    return str;
+}
+
 struct ClientHandler final : public Napi::ObjectWrap<ClientHandler> {
 public:
     static Napi::Object init(Napi::Env env, Napi::Object exports)
@@ -67,12 +108,16 @@ private:
             request = json_stringify(env, info[0].As<Napi::Object>()).Utf8Value();
         }
 
-        auto slice = tonlib::ClientJson::execute(td::Slice{request.data(), request.size()});
-        if (slice.empty()) {
+        auto r_request = to_request(request);
+        if (r_request.is_error()) {
+            const auto message = PSLICE() << "Failed to parse " << tag("request", td::format::escaped(request)) << " " << r_request.error();
+            Napi::Error::New(env, message.c_str()).ThrowAsJavaScriptException();
             return env.Null();
         }
 
-        return Napi::String::New(env, slice.data(), slice.size());
+        return Napi::String::New(
+            env,
+            from_response(*tonlib::Client::execute(tonlib::Client::Request{0, std::move(r_request.ok_ref().first)}).object, r_request.ok().second));
     }
 
     void send(const Napi::CallbackInfo& info)
@@ -84,7 +129,20 @@ private:
         }
 
         auto request = json_stringify(env, info[0].As<Napi::Object>()).Utf8Value();
-        client_json_.send(td::Slice{request.data(), request.size()});
+
+        auto r_request = to_request(request);
+        if (r_request.is_error()) {
+            const auto message = PSLICE() << "Failed to parse " << tag("request", td::format::escaped(request)) << " " << r_request.error();
+            Napi::Error::New(env, message.c_str()).ThrowAsJavaScriptException();
+            return;
+        }
+
+        std::uint64_t extra_id = extra_id_.fetch_add(1, std::memory_order_relaxed);
+        if (!r_request.ok_ref().second.empty()) {
+            std::lock_guard<std::mutex> guard(mutex_);
+            extra_[extra_id] = std::move(r_request.ok_ref().second);
+        }
+        client_.send(tonlib::Client::Request{extra_id, std::move(r_request.ok_ref().first)});
     }
 
     auto receive(const Napi::CallbackInfo& info) -> Napi::Value
@@ -95,15 +153,28 @@ private:
             return Napi::Value{};
         }
 
-        auto slice = client_json_.receive(info[0].As<Napi::Number>().DoubleValue());
-        if (slice.empty()) {
+        auto response = client_.receive(info[0].As<Napi::Number>().DoubleValue());
+        if (!response.object) {
             return env.Null();
         }
 
-        return json_parse(env, Napi::String::New(env, slice.data(), slice.size()));
+        std::string extra;
+        if (response.id != 0) {
+            std::lock_guard<std::mutex> guard(mutex_);
+            auto it = extra_.find(response.id);
+            if (it != extra_.end()) {
+                extra = std::move(it->second);
+                extra_.erase(it);
+            }
+        }
+
+        return json_parse(env, Napi::String::New(env, from_response(*response.object, extra)));
     }
 
-    tonlib::ClientJson client_json_;
+    tonlib::Client client_;
+    std::mutex mutex_;  // for extra_
+    std::unordered_map<std::int64_t, std::string> extra_;
+    std::atomic<std::uint64_t> extra_id_{1};
 };
 
 Napi::Object init(Napi::Env env, Napi::Object exports)
---
+++ tests/client-test.js
@@ -0,0 +1,63 @@
+const NativeTonlibClient = require('../build/lib/tonlib-js.abi-83').TonlibClient;
+
+const CONFIG = `{
+  "liteservers": [
+    {
+      "ip": 916349379,
+      "port": 3031,
+      "id": {
+        "@type": "pub.ed25519",
+        "key": "uNRRL+6enQjuiZ/s6Z+vO7yxUUR7uxdfzIy+RxkECrc="
+      }
+    }
+  ],
+  "validator": {
+     "@type": "validator.config.global",
+     "zero_state": {
+        "workchain": -1,
+        "shard": -9223372036854775808,
+        "seqno": 0,
+        "root_hash": "WP/KGheNr/cF3lQhblQzyb0ufYUAcNM004mXhHq56EU=",
+        "file_hash": "0nC4eylStbp9qnCq8KjDYb789NjS25L5ZA1UQwcIOOQ="
+     }
+  }
+}`;
+
+class TestClient {
+  constructor() {
+    this.tonlibClient = new NativeTonlibClient();
+  }
+
+  init() {
+    this.tonlibClient.send({
+      '@type': 'init',
+      options: {
+        config: {
+          config: CONFIG,
+          blockchain_name: 'mainnet'
+        },
+        keystore_type: {
+          '@type': 'keyStoreTypeInMemory'
+        }
+      }
+    })
+  }
+
+  getMasterchainInfo() {
+    this.tonlibClient.send({
+      '@type': 'liteServer.getMasterchainInfo'
+    })
+  }
+
+  poll(timeout) {
+    console.log(this.tonlibClient.receive(timeout));
+  }
+}
+
+const client = new TestClient();
+client.init();
+client.getMasterchainInfo();
+
+while (true) {
+  console.log(client.poll(300));
+}
