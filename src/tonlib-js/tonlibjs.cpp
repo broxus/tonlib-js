72ef57440280a776abf6621a044ff19a5dd8c00c
Ivan Kalinin <i.kalinin@dexpa.io>
2020-10-31 19:07:23 +0300

    Add napi

--- src/tonlib-js/tonlibjs.cpp
+++ src/tonlib-js/tonlibjs.cpp
@@ -1,5 +1,22 @@
 #include "tonlibjs.hpp"
 
+#include <napi.h>
+#include <nan.h>
+
 namespace tjs
 {
+Napi::String hello(const Napi::CallbackInfo& info)
+{
+    auto env = info.Env();
+    return Napi::String::New(env, "world");
+}
+
+Napi::Object Init(Napi::Env env, Napi::Object exports)
+{
+    exports.Set(Napi::String::New(env, "hello"), Napi::Function::New(env, hello));
+    return exports;
+}
+
 }  // namespace tjs
+
+NODE_API_MODULE(tonlib_js, tjs::Init)
