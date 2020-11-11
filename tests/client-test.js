2057ab75d7851da6ed194f2a871277819e09ffce
Ivan Kalinin <i.kalinin@dexpa.io>
2020-11-11 21:28:55 +0300

    Update ton dependency

--- tests/client-test.js
+++ tests/client-test.js
@@ -1,4 +1,5 @@
 const tl = require('..');
+const msigAbi = JSON.stringify(require('./msig.abi.json'));
 
 const CONFIG = `{
   "liteservers": [
@@ -45,6 +46,27 @@ class TestClient {
   getMasterchainInfo() {
     return this.tonlibClient.send(new tl.LiteServerGetMasterchainInfo());
   }
+
+  fnGetCustodians() {
+    return this.tonlibClient.send(new tl.FtabiGetFunction({
+      abi: msigAbi,
+      name: 'getCustodians'
+    }));
+  }
+
+  generateKeyPair() {
+    return this.tonlibClient.send(new tl.GenerateKeyPair());
+  }
+
+  runLocal(accountAddress, fn, call) {
+    return this.tonlibClient.send(new tl.FtabiRunLocal({
+      address: new tl.AccountAddress({accountAddress}),
+      function: fn,
+      call: new tl.FtabiFunctionCallJson({
+        value: JSON.stringify(call)
+      })
+    }));
+  }
 }
 
 const client = new TestClient();
@@ -55,6 +77,19 @@ const client = new TestClient();
 
     const state = await client.getMasterchainInfo();
     console.log(state.last._props);
+
+    const fnGetCustodians = await client.fnGetCustodians();
+    console.log(fnGetCustodians._props);
+
+    const keyPair = await client.generateKeyPair();
+    console.error(keyPair._props);
+
+    const result = await client.runLocal('0:2e4492152c323667733ba555ad0165642ae7e5e346e6b1077ab6866ae39a3dc3', fnGetCustodians, {
+      header: {
+        pubkey: Buffer.from(keyPair.publicKey).toString('base64')
+      }
+    });
+    console.log(result);
   } catch (e) {
     console.error(e);
   }
